18c8b7be06e095d8796d20f6f0296fabefa128a2
dcuevas24 <ivandaniel1224@gmail.com>
2021-08-05 12:16:36 -0500

    Segundo taller del curso terminado

---
+++ descuentos.js
@@ -0,0 +1,25 @@
+const precioOriginal = 120;
+const descuento = 18;
+
+function calcularPrecioConDescuento(precio, descuento) {
+  const porcentajePrecioConDescuento = 100 - descuento;
+  const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
+
+  return precioConDescuento;
+}
+
+function onClickButtonPriceDiscount() {
+  const inputPrice = document.getElementById("inputPrice");
+  const priceValue = Number(inputPrice.value);
+
+  const inputDiscount = document.getElementById("inputDiscount");
+  const discountValue = Number(inputDiscount.value);
+
+  const precioConDescuento = calcularPrecioConDescuento(
+    priceValue,
+    discountValue
+  );
+
+  const resultPrice = document.getElementById("resultPrice");
+  resultPrice.innerText = "El precio con descuento son: $" + precioConDescuento;
+}
