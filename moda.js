debc8da96e691c95a65c4b0b6a5f566d8c2aac2f
dcuevas24 <ivandaniel1224@gmail.com>
2021-08-10 11:32:12 -0500

    Reto de moda terminado

---
+++ moda.js
@@ -0,0 +1,20 @@
+const lista1 = [1, 2, 3, 1, 2, 3, 4, 5, 1, 2, 2, 2];
+
+const lista1Count = {};
+
+lista1.map(function (elemento) {
+  if (lista1Count[elemento]) {
+    lista1Count[elemento] += 1;
+  } else {
+    lista1Count[elemento] = 1;
+  }
+});
+
+const lista1Array = Object.entries(lista1Count).sort(function (
+  valorAcumulado,
+  nuevoValor
+) {
+  return valorAcumulado[1] - nuevoValor[1];
+});
+
+const moda = lista1Array[lista1Array.length - 1];
