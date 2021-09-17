0b842aac08fd60ec30a801c9d1cfa920cc3141a9
dcuevas24 <ivandaniel1224@gmail.com>
2021-09-17 11:46:32 -0500

    Variable de precios y descuento eleminados

--- descuentos.js
+++ descuentos.js
@@ -1,6 +1,3 @@
-const precioOriginal = 120;
-const descuento = 18;
-
 function calcularPrecioConDescuento(precio, descuento) {
   const porcentajePrecioConDescuento = 100 - descuento;
   const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
